5fd877c4cdd3534e42f3617b8d192e5caf889d1b
Rajat47485thakur <121743303+Rajat47485thakur@users.noreply.github.com>
2024-03-12 16:34:08 +0530

    on 12Mar at Apptunix I Prectices on the utils Api (req, res , next) and the errors utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+//--------------This is for the Promise-----------
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+};
+
+
+
+export { asyncHandler }
+
+
+
+
+
+//------------This is for try catch---------------
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
