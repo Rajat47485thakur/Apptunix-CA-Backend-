7fe0fcaa2274df50d728fa77f7468f79abd51f02
Rajat47485thakur <121743303+Rajat47485thakur@users.noreply.github.com>
2024-03-13 12:41:48 +0530

    backend project

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_COLUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" })
+        //file has been uploaded successfully
+        console.log('Upload to Cloudinary Successful', response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);//delete local saved file after error in uploading  it to clodinary 
+        return null
+    }
+}
+
+
+export { uploadOnCloudinary };
